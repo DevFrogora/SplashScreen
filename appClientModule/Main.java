596f24942e6a03ec8e1c0a6609750e8e659d3ef1
neverDieAgain <rr4428310@gmail.com>
2021-12-31 17:53:16 +0530

    old project

---
+++ appClientModule/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		SplashDemo test = new SplashDemo();
+	}
+
+	/* (non-Java-doc)
+	 * @see java.lang.Object#Object()
+	 */
+	public Main() {
+		super();
+		
+	}
+
+}
